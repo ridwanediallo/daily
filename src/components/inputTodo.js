6dc005fa3b9b42f11ef74b964db7c52e77053055
ridwan <ridwanediallo99@gmail.com>
2022-12-18 22:39:34 +0000

    Add inputTodo component

---
+++ src/components/inputTodo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const InputTodo = () => {
+  return (
+    <div className="self-center">
+      <form className='flex'>
+        <input className="border py-2 w-full" type="text" name="name" />
+        <input
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default InputTodo;
