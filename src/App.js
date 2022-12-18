f2dac618caa5cdcd2a540929d55eecffb9a689df
ridwan <ridwanediallo99@gmail.com>
2022-12-18 21:22:42 +0000

    Add project title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 function App() {
   return (
-    <>
-     
-    </>
+    <div className="flex justify-center h-screen mt-40">
+      <h1 className="self-csnter text-3xl text-sky-500">Daily tasks</h1>
+    </div>
   );
 }
 
