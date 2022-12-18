9da8724153a08fc488b41d15d6ed36ef6b867a2e
ridwan <ridwanediallo99@gmail.com>
2022-12-18 21:30:43 +0000

    Add folder structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="flex justify-center h-screen mt-40">
-      <h1 className="self-csnter text-3xl text-sky-500">Daily tasks</h1>
+      <h1 className="text-3xl text-sky-500">Daily tasks</h1>
     </div>
   );
 }
