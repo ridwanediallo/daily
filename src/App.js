8db995ecde885de04719d98898cbad77d95fb39f
ridwan <ridwanediallo99@gmail.com>
2022-12-18 22:42:17 +0000

    Import OutputTodo component to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import OutputTodo from "./components/outputTodo";
 
 function App() {
   return (
-    <div className="flex justify-center h-screen mt-40">
-      <h1 className="text-3xl text-sky-500">Daily tasks</h1>
+    <div className="flex justify-center mt-40">
+      <OutputTodo />
     </div>
   );
 }
