3283d6298f09b3caf1233b8bdac0b9804be08063
ridwan <ridwanediallo99@gmail.com>
2022-12-19 16:55:06 +0000

    Imprt AuthContext

--- src/components/inputTodo.js
+++ src/components/inputTodo.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import { useContext } from 'react';
+import AuthContext from '../context/auth-context';
 
 const InputTodo = () => {
+  const todoContext = useContext(AuthContext);
+
   return (
     <div className="self-center">
-      <form className='flex'>
+      <form className="flex">
         <input className="border py-2 w-full" type="text" name="name" />
         <input
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
